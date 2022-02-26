3abf3677e20250e6a15b2e84facab3a610cbaeff
kun881225 <99487726+kun881225@users.noreply.github.com>
2022-02-26 19:02:29 +0800

    add index and Daily 1

---
+++ Daily UI/Day3 Landing page/day3LandingPage.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    let menuToggle = document.querySelector(".toggle");
+    let menu = document.querySelector(".menu");
+    menuToggle.classList.toggle("active");
+    menu.classList.toggle("active")
+}
