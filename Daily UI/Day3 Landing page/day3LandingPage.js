48fc1aa60e64b2fa67f263d07ef09c737b79fa45
kun881225 <99487726+kun881225@users.noreply.github.com>
2022-03-12 14:37:30 +0800

    DailyUIDay3

--- Daily UI/Day3 Landing page/day3LandingPage.js
+++ Daily UI/Day3 Landing page/day3LandingPage.js
@@ -1,6 +1,8 @@
 function toggleMenu() {
     let menuToggle = document.querySelector(".toggle");
     let menu = document.querySelector(".menu");
+    let logo = document.querySelector("section .logo");
     menuToggle.classList.toggle("active");
-    menu.classList.toggle("active")
+    menu.classList.toggle("active");
+    logo.classList.toggle("active");
 }
